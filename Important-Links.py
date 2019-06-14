ebe11a77ef6b340215b05e0d63b3f61d336524c8
Ujjwal Baid <ujjwalbaid0408@gmail.com>
2019-06-14 11:17:41 +0530

    Important Implementation Links
    Some important GitHub links

---
+++ Important-Links.py
@@ -0,0 +1,8 @@
+Some important links for implementation:
+
+
+https://github.com/jadevaibhav/Brain-Tumor-Segmentation-using-Deep-Neural-networks/blob/master/model.ipynb
+https://github.com/dalmia/Brain-Tumor-Segmentation-Keras/blob/master/MultichannelCascadeCNN.ipynb
+https://github.com/tkuanlun350/3DUnet-Tensorflow-Brats18
+https://github.com/polo8214/Brain-tumor-segmentation-using-deep-learning
+https://github.com/Jack-Etheredge/Brain-Tumor-Segmentation-3D-UNet-CNN
